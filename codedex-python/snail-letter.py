db6b050fef5095627c56aa6a5f1829fe19ed720d
jacquelinemathias <jacqueline.mathias38@gmail.com>
2025-04-19 17:04:09 -0300

    Exercício snail letter em Python

---
+++ codedex-python/snail-letter.py
@@ -0,0 +1,13 @@
+# Use print() to output:
+
+#    Today's date.
+#    How you are feeling right now.
+#   What you want to accomplish by learning to code.
+#A little message to your older, wiser, and programmer self.
+#    Your favorite emoji to spice things up!
+
+print('19/04/2025')
+print('Estou me sentindo satisfeita com o progresso que estou fazendo. Me sinto privilegiada e amada por todo o apoio que recebo de amigos e familiares que querem que eu vença. Me sinto grata pela oportunidade que consegui de estagiar novamente na área de tecnologia.')
+print('Quero conseguir codar sem consultar muita coisa, quero ter muito conhecimento fixado fortemente na minha cabeça.')
+print('Espero que tenha conseguido se destacar muito na área de desenvolvimento, espero que tenha conseguido o home office que sempre quis e um marido amoroso!')
+print('nhaw 🐉✨')
